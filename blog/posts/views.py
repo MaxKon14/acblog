2cca23443837959263950b9925b41560fc58f6fa
Коновалов Максим <Konovalov_M_A@itgri.ru>
2026-01-29 12:26:12 +0300

    1.15 filter by is_published

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer, PostSerializer, CategorySerializer, SubscriberSerializer
 from .models import Post, Category, Subscribers
@@ -13,11 +14,18 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class PostsViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
+    queryset = queryset.filter(
+        is_published=True,
+        pub_date__lte=timezone.now()
+    ).exclude(category__is_published=False).exclude(category__isnull=True).distinct()
     serializer_class = PostSerializer
     lookup_field = 'slug'
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
+    queryset = queryset.filter(
+        is_published=True,
+    )
     serializer_class = CategorySerializer
     lookup_field = 'slug'
 
