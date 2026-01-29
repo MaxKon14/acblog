c8f194fa5cc4edeca6f0b8606def52642a97f193
Коновалов Максим <Konovalov_M_A@itgri.ru>
2026-01-29 11:15:23 +0300

    1.11 add token authefication

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -139,5 +139,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ) + (('rest_framework.renderers.BrowsableAPIRenderer',) if DEBUG else ()),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
